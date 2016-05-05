7812d988b55e35e5ceea35b3a041681860338dd8
aridoza <aridoza85@gmail.com>
2016-05-05 12:08:33 -0400

    crud sample complete

--- Offroad/backend/item_cart/app/assets/javascripts/components/_body.js.jsx
+++ Offroad/backend/item_cart/app/assets/javascripts/components/_body.js.jsx
@@ -3,15 +3,48 @@ var Body = React.createClass({
     return { items: [] }
   },
   componentDidMount() {
-    console.log("Component mounted");
     $.getJSON('/api/v1/items.json', (response) => { this.setState({ items: response }) });
   },
+  handleDelete(id) {
+    $.ajax({
+      url: '/api/v1/items/${id}',
+      type: 'DELETE',
+      success: () => {
+        this.removeItemClient(id);
+      }
+    });
+  },
+  removeItemClient(id) {
+    var newItems = this.state.items.filter((item) => {
+      return item.id != id;
+    });
+    this.setState({ items: newItems });
+  },
+  handleUpdate(item) {
+    $.ajax({
+      url: '/api/v1/items/${item.id}',
+      type: 'PUT',
+      data: { item: item },
+      success: () => {
+        this.updateItems(item);
+      }
+    })
+  },
+
+  updateItems(item) {
+    var items = this.state.items.filter((i) => { return i.id != item.id });
+    items.push(item);
+
+    this.setState({ items: items });
+  },
+
+
 
   render(){
     return(
       <div>
-        <NewItem />
-        <AllItems items={this.state.items}/>
+        <NewItem handleSubmit={this.handleSubmit}/>
+        <AllItems items={this.state.items} handleDelete={this.handleDelete} onUpdate={this.handleUpdate}/>
       </div>
     )
   }
--- Offroad/backend/item_cart/app/assets/javascripts/components/_new_item.js.jsx
+++ Offroad/backend/item_cart/app/assets/javascripts/components/_new_item.js.jsx
@@ -2,13 +2,12 @@ var NewItem = React.createClass({
   handleClick() {
     var name = this.refs.name.value;
     var description = this.refs.description.value;
-    console.log("the name value is" + name + "the description value is" + description);
     $.ajax({
       url: '/api/v1/items',
       type: 'POST',
       data: { item: { name: name, description: description } },
-      success: (response) => {
-        console.log("ajax response:", response);
+      success: (item) => {
+        this.props.handleSubmit(item);
       }
     });
   },
