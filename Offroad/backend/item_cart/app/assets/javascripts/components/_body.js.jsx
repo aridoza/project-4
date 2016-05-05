91e96ad1056d73d1dbaebebcc55d95b85478b69c
aridoza <aridoza85@gmail.com>
2016-05-05 09:51:46 -0400

    parkr-app initial setup

--- Offroad/backend/item_cart/app/assets/javascripts/components/_body.js.jsx
+++ Offroad/backend/item_cart/app/assets/javascripts/components/_body.js.jsx
@@ -6,11 +6,30 @@ var Body = React.createClass({
     console.log("Component mounted");
     $.getJSON('/api/v1/items.json', (response) => { this.setState({ items: response }) });
   },
+  handleSubmit(item) {
+    var newState = this.state.items.concat(item);
+    this.setState({ items: newState })
+  },
+  handleDelete(id) {
+    $.ajax({
+      url: '/api/v1/items/${id}',
+      type: 'DELETE',
+      success: () => {
+        this.removeItemClient(id);
+      }
+    });
+  },
+  removeItemClient(id) {
+    var newItems = this.state.items.filter((item) => {
+      return item.id != id;
+    });
+    this.setState({ items: newItems })
+  },
 
   render(){
     return(
       <div>
-        <NewItem />
+        <NewItem handleSubmit={this.handleSubmit}/>
         <AllItems items={this.state.items}/>
       </div>
     )
